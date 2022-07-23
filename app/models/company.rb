541556a21fba8c108d821fc317b5480d6dcb1586
NicMortelliti <npmortelliti@gmail.com>
2022-07-23 10:51:07 -0700

    Return oldest company

--- app/models/company.rb
+++ app/models/company.rb
@@ -14,4 +14,13 @@ class Company < ActiveRecord::Base
       dev_id: dev,
       company_id: self.id )
   end
+
+  # Return oldest company (CLASS method)
+  def self.oldest_company
+    oldest = Company.find_by(founding_year: Company.all.pluck(:founding_year).min)
+
+    # Deliverables unclear. Printing the answer then returning it.
+    puts oldest
+    oldest
+  end
 end
