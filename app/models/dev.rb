a3997830ad8c1c84802e41d044480b6d09cc01a5
NicMortelliti <npmortelliti@gmail.com>
2022-07-23 11:07:16 -0700

    Add Received One method

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -5,4 +5,11 @@ class Dev < ActiveRecord::Base
 
   # Allows for Dev#companies
   has_many :companies, through: :freebies
+
+  # Received a freebie?
+  def received_one?(item_name)
+    # Pluck will give us an array of item_names
+    # Then, we check if that array includes item_name parameter
+    self.freebies.pluck(:item_name).include?(item_name)
+  end
 end
