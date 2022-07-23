c1f1b6b40eee61f36a68428bed4eaf414f135a38
NicMortelliti <npmortelliti@gmail.com>
2022-07-23 10:05:26 -0700

    Add freebie seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,13 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Car", value: 42000, company_id: 4, dev_id: 1)
+Freebie.create(item_name: "Pen", value: 1, company_id: 3, dev_id: 2)
+Freebie.create(item_name: "Airplane", value: 40000, company_id: 1, dev_id: 3)
+Freebie.create(item_name: "Bagel", value: 100000, company_id: 2, dev_id: 4)
 
 puts "Seeding done!"
