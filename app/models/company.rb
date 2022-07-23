d7b81edc16300b768b0baf8cbf45ff11e9d3e9b0
NicMortelliti <npmortelliti@gmail.com>
2022-07-23 10:32:00 -0700

    Add Give Freebie method

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,13 @@ class Company < ActiveRecord::Base
 
   # Allows for Company#devs
   has_many :devs, through: :freebies
+
+  # Give Freebie
+  def give_freebie(dev, item_name, value)
+    Freebie.create(
+      item_name: item_name,
+      value: value,
+      dev_id: dev,
+      company_id: self.id )
+  end
 end
