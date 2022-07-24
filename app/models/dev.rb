df8723fc67a990a4ced22c72e82bac54bbf04f35
NicMortelliti <npmortelliti@gmail.com>
2022-07-24 13:24:44 -0700

    Add Give Away method

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -12,4 +12,25 @@ class Dev < ActiveRecord::Base
     # Then, we check if that array includes item_name parameter
     self.freebies.pluck(:item_name).include?(item_name)
   end
+
+  # Give away freebie
+  def give_away(dev, freebie)
+
+    # Freebie can only be given away by
+    # dev who currently has it
+    if received_one?(freebie) == true
+      
+      # Changes freebies dev to be the given dev
+      freebie_full = Freebie.find_by(item_name: freebie)
+
+      freebie_full.dev_id = Dev.find_by(name: dev).id
+      freebie_full.save
+
+      puts "#{self.name} gave their #{freebie} to #{dev}."
+
+    else
+      puts "#{self.name} has no #{freebie} to give away."
+    end
+
+  end
 end
